32ca0b22a5d482bd8c96b8259158a4a6f7a4a660
Samuel MP <samuelmp1919@gmail.com>
2020-08-21 14:32:10 -0600

    Revert "asdf"
    This reverts commit 9a4c30d6ce6ea007de9ddbe1bd3e212061830da1.

--- Practica para Examen Final/DAL/Conection.cs
+++ Practica para Examen Final/DAL/Conection.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DO;
 
 namespace DAL
 {
@@ -12,31 +11,9 @@ namespace DAL
     {
         public IMongoDatabase ConectarConBaseDeDatos()
         {
-            var client = new MongoClient("mongodb+srv://root:root@myatlascluster-wregg.gcp.mongodb.net/sample_airbnb?retryWrites=true&w=majority");
+            var client = new MongoClient("mongodb+srv://root:root@myatlascluster-wregg.gcp.mongodb.net/Veterinaria?retryWrites=true&w=majority");
             var database = client.GetDatabase("sample_airbnb");
             return database;
         }
-
-        public airbnb airbnbPorNombre(string nombreAirbnb)
-        {
-            var laBaseDeDatos = ConectarConBaseDeDatos();
-            var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
-            var expresssionFilter = Builders<airbnb>.Filter.Eq(x => x.name, nombreAirbnb);
-            var elResultado = collection.Find(expresssionFilter).FirstOrDefault();
-            return elResultado;
-        }
-
-        public airbnb airbnbRangoPrecio(decimal precioIncial, decimal precioFinal)
-        {
-            var laBaseDeDatos = ConectarConBaseDeDatos();
-            var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
-            var expresssionFilter = Builders<airbnb>.Filter.Where(x => x.price >= precioIncial && x.price <= precioFinal);
-            var elResultado = collection.Find(expresssionFilter).FirstOrDefault();
-            return elResultado;
-        }
-
-
-
-
     }
 }
