0a48cfc342de2c22e0bbab66167dee51548fa9c5
Jean Carlos Quesada Cascante <jc.qc1230@gmail.com>
2020-08-21 15:46:55 -0600

    no message

--- Practica para Examen Final/DAL/Connection.cs
+++ Practica para Examen Final/DAL/Connection.cs
@@ -17,21 +17,82 @@ namespace DAL
             return database;
         }
 
-        public IList<airbnb> ConteinFacility()
+        public airbnb airbnbPorNombre(string elNombreAirbnb)
         {
             var laBaseDeDatos = ConectarConBaseDeDatos();
             var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
-            var expresssionFilter = Builders<airbnb>.Filter.Where(s => s.amenities.Contains("facility"));
+            var expresssionFilter = Builders<airbnb>.Filter.Eq(x => x.name, elNombreAirbnb);
+            var elResultado = collection.Find(expresssionFilter).FirstOrDefault();
+            return elResultado;
+        }
+
+        public IList<airbnb> ConteinFacility(String facility)
+        {
+            var laBaseDeDatos = ConectarConBaseDeDatos();
+            var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
+            var expresssionFilter = Builders<airbnb>.Filter.Where(s => s.amenities.Contains(facility));
             var elResultado = collection.Find(expresssionFilter).ToList();
             return elResultado;
         }
 
-        public IList<airbnb> aux()
+        public List<airbnb> airbnbRangoPrecio(decimal precioInicial, decimal precioFinal)
         {
             var laBaseDeDatos = ConectarConBaseDeDatos();
             var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
-            var elResultado = collection.Find(_ => true).ToList();
+            var expresssionFilter = Builders<airbnb>.Filter.Where(s => s.price >= precioInicial && s.price <= precioFinal);
+            var elResultado = collection.Find(expresssionFilter).ToList();
             return elResultado;
         }
+
+        public void AgregarAmenities(String objeto, String elNombreAirbnb)
+        {
+            var airbnb = airbnbPorNombre(elNombreAirbnb);
+            airbnb.amenities.Add(objeto);
+            var laBaseDeDatos = ConectarConBaseDeDatos();
+            var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
+            var builder = Builders<airbnb>.Filter;
+            var filter = builder.Eq(s => s.name, elNombreAirbnb);
+            var update = Builders<airbnb>.Update
+             .Set(d => d.amenities, airbnb.amenities);
+            UpdateResult result = collection.UpdateOne(filter, update);
+        }
+
+        public void AgregarReviews(String name,DateTime date,String listing_id,String reviewer_id,String reviewer_name, String comments)
+        {
+            Random rnd = new Random();
+            int idRandom = rnd.Next(1, 896489598);
+
+            var airbnb = airbnbPorNombre(name);
+            Reviews reviews = new Reviews();
+            reviews._id = idRandom.ToString();
+            reviews.date = date;
+            reviews.listing_id = listing_id;
+            reviews.reviewer_id = reviewer_id;
+            reviews.reviewer_name = reviewer_name;
+            reviews.comments = comments;
+
+            airbnb.reviews.Add(reviews);
+            var database = ConectarConBaseDeDatos();
+            var collection = database.GetCollection<airbnb>("listingsAndReviews");
+            var builder = Builders<airbnb>.Filter;
+            var filter = builder.Eq(s => s.name, name);
+            var update = Builders<airbnb>.Update
+             .Set(d => d.reviews, airbnb.reviews);
+            UpdateResult result = collection.UpdateOne(filter, update);
+        }
+
+        public void UpdateComments(String newComentario, String elNombreAirbnb, string idReview)
+        {
+            var airbnb = airbnbPorNombre(elNombreAirbnb);
+            airbnb.reviews.Where(s => s._id == idReview).FirstOrDefault().comments = newComentario;
+            var laBaseDeDatos = ConectarConBaseDeDatos();
+            var collection = laBaseDeDatos.GetCollection<airbnb>("listingsAndReviews");
+            var builder = Builders<airbnb>.Filter;
+            var filter = builder.Eq(s => s.name, elNombreAirbnb);
+            var update = Builders<airbnb>.Update
+             .Set(d => d.reviews, airbnb.reviews);
+            UpdateResult result = collection.UpdateOne(filter, update);
+        }
+
     }
 }
--- Practica para Examen Final/Practica para Examen Final/Worker.cs
+++ Practica para Examen Final/Practica para Examen Final/Worker.cs
@@ -19,56 +19,160 @@ namespace Practica_para_Examen_Final
                 switch (laOpcion)
                 {
                     case "1":
-
+                        Console.WriteLine("Digite el nombre del airbnb");
+                        String name = Console.ReadLine();
+                        BuscarPorNombre(name);
                         break;
                     case "2":
-                        ConteinFacility();
+                        Console.WriteLine("Digite el nombre del airbnb");
+                        String facility = Console.ReadLine();
+                        ConteinFacility(facility);
                         break;
                     case "3":
-                        
+                        Console.WriteLine("Precio Inicial");
+                        Decimal precioInicial = Decimal.Parse(Console.ReadLine());
+                        Console.WriteLine("Precio Final");
+                        Decimal precioFinal = Decimal.Parse(Console.ReadLine());
+                        airbnbRangoPrecio(precioInicial, precioFinal);
                         break;
                     case "4":
-                      
+                        Console.WriteLine("Digite el nombre del airbnb");
+                        String nombreairbnb = Console.ReadLine();
+                        Console.WriteLine("digite la Amenities");
+                        String Amenities = Console.ReadLine();
+                        AgregarAmenities(Amenities, nombreairbnb);
                         break;
                     case "5":
-                       
+                        Console.WriteLine("Digite el nombre del airbnb");
+                        String nombre = Console.ReadLine();
+                        Console.WriteLine("digite el listing_id");
+                        String listing_id = Console.ReadLine();
+                        Console.WriteLine("digite el reviewer_id");
+                        String reviewer_id = Console.ReadLine();
+                        Console.WriteLine("digite el reviewer_name");
+                        String reviewer_name = Console.ReadLine();
+                        Console.WriteLine("digite el comments");
+                        String comments = Console.ReadLine();
+                        AgregarReviews(nombre, listing_id, reviewer_id, reviewer_name, comments);
                         break;
                     case "6":
-                        
+                        Console.WriteLine("Digite el nombre del airbnb");
+                        String airbnbName = Console.ReadLine();
+                        BuscarPorReviews(airbnbName);
+                        Console.WriteLine("digite el id de la review");
+                        String id = Console.ReadLine();
+                        Console.WriteLine("digite el nuevo comentario");
+                        String comentario = Console.ReadLine();
+                        UpdateComments(comentario, airbnbName, id);
                         break;
                     default:
                         break;
                 }
             }
         }
-        //private void BuscarPorNombre(String nombre)
-        //{
-        //    var client = new Connection();
-        //    var airbnb = client.ConteinFacility(nombre);
-        //    Console.WriteLine(string.Format("Id: {1}; Nombre: {0};",
-        //        airbnb.Nombre, animalito.AnimalitoId.ToString(), animalito.ElPropietario.Nombre, animalito.ElPropietario.DireccionElectronica));
-        //}
-
-        private void ConteinFacility()
+        private void BuscarPorNombre(String nombre)
         {
             var client = new Connection();
-            var airbnb = client.aux();
-            foreach (var tmp in airbnb)
+            var airbnb = client.airbnbPorNombre(nombre);
+            if (airbnb != null)
             {
-                Console.WriteLine(string.Format("Id: {0}; Nombre: {1}; Amenities{2}",
-              tmp._id, tmp.name, String.Join(" / ",tmp.amenities)));
+                Console.WriteLine(string.Format("Id: {0}; Nombre: {1};",
+                    airbnb._id, airbnb.name));
+            }
+            else{
+                Console.WriteLine("No se encontraron airbnb");
             }
         }
 
+        private void ConteinFacility(String facility)
+        {
+            var client = new Connection();
+            var airbnb = client.ConteinFacility(facility);
+
+            if (airbnb.Count != 0) {
+                foreach (var tmp in airbnb)
+                {
+                    Console.WriteLine(string.Format("Id: {0}; Nombre: {1}; Amenities{2}",
+                  tmp._id, tmp.name, String.Join(" / ", tmp.amenities)));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron airbnb");
+            }
+        }
+
+        private void airbnbRangoPrecio(Decimal precioInicial, Decimal precioFinal)
+        {
+            var client = new Connection();
+            var airbnb = client.airbnbRangoPrecio(precioInicial, precioFinal);
+
+            if (airbnb.Count != 0)
+            {
+                foreach (var tmp in airbnb)
+                {
+                    Console.WriteLine(string.Format("Id: {0}; Nombre: {1}; precio{2}",
+                  tmp._id, tmp.name, tmp.price));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron airbnb");
+            }
+        }
+
+        private void AgregarAmenities(String objeto, String elNombreAirbnb)
+        {
+            var client = new Connection();
+            DateTime date = DateTime.Now;
+            client.AgregarAmenities(objeto, elNombreAirbnb);
+
+            Console.WriteLine("Se agrego la Amenities");
+        }
+
+        private void AgregarReviews(String name,String listing_id, String reviewer_id, String reviewer_name, String comments)
+        {
+            var client = new Connection();
+            DateTime date = DateTime.Now;
+            client.AgregarReviews(name, date, listing_id, reviewer_id, reviewer_name, comments);
+
+            Console.WriteLine("Se agrego la review");
+        }
+
+        private void UpdateComments(String newComentario, String elNombreAirbnb, string idReview)
+        {
+            var client = new Connection();
+            client.UpdateComments(newComentario, elNombreAirbnb, idReview);
+
+            Console.WriteLine("Se agrego la review");
+        }
+
+        private void BuscarPorReviews(String nombreAirbnb)
+        {
+            var client = new Connection();
+            var airbnb = client.airbnbPorNombre(nombreAirbnb);
+            if (airbnb != null)
+            {
+                foreach (var aux in airbnb.reviews)
+                {
+                    Console.WriteLine(string.Format("Id: {0}; Nombre: {1};",
+                        aux._id, aux.reviewer_name));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron airbnb");
+            }
+        }
 
         private void DesplegarMenu()
         {
             Console.WriteLine("Menu Principal");
             Console.WriteLine("1.  Buscar por nombre");
             Console.WriteLine("2.  Listado de todos los registros que contienen un facility en amenity");
-            Console.WriteLine("3.  Agregar Contacto");
-            Console.WriteLine("4.  Eliminar Contacto");
-            Console.WriteLine("5.  Agregar Vacuna");
+            Console.WriteLine("3.  Listado de todos los registros por rango de precio.");
+            Console.WriteLine("4.  Agregar Amenities");
+            Console.WriteLine("5.  Agregar Review");
             Console.WriteLine("6.  Lista Animales");
             Console.WriteLine("X.  Salir");
         }
